47fd036a0793c188f2ab560b3d57e298d869a3c1
Juan Carlos Lozada Castillo <jc.lozadacastillo@outlook.com>
2023-02-08 12:11:04 -0500

    Terminado formulario inscripción

--- ISTPET_PortalEstudiantil/Controllers/InscripcionesController.cs
+++ ISTPET_PortalEstudiantil/Controllers/InscripcionesController.cs
@@ -1,21 +1,98 @@
-﻿using ISTPET_PortalEstudiantil.Models.sigafi_es;
+﻿using Dapper;
+using ISTPET_PortalEstudiantil.Models.sigafi_es;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 using System;
 
 namespace ISTPET_PortalEstudiantil.Controllers
 {
     public class InscripcionesController : Controller
     {
+        private readonly string cn;
+        private readonly string condicionPruebas="and datediff(p.fecha_inicial, current_date()) >-100";
+        public InscripcionesController(IConfiguration config)
+        {
+            cn = config.GetConnectionString("sigafi_es");
+        }
         public IActionResult Index()
         {
             return View();
         }
-//select distinct(c.idCarrera),c.Carrera
-//from cursos n
-//inner join carreras c on c.idCarrera = n.idCarrera
-//inner join periodos_matriculas_niveles mn on mn.idNivel = n.idNivel
-//inner join periodos p on p.idPeriodo = mn.idPeriodo
-//where c.activa = 1
-//and datediff(p.fecha_inicial, current_date()) >-100
+        [HttpGet]
+        public async Task<IActionResult> comboCarreras()
+        {
+            var dapper=new MySqlConnection(cn);
+            try
+            {
+                string sql=$@"select distinct(c.idCarrera),c.Carrera
+                            from cursos n
+                            inner join carreras c on c.idCarrera = n.idCarrera
+                            inner join periodos_matriculas_niveles mn on mn.idNivel = n.idNivel
+                            inner join periodos p on p.idPeriodo = mn.idPeriodo
+                            where c.activa = 1 and p.permiteMatricula=1
+                            {condicionPruebas}";
+                return Json(await dapper.QueryAsync(sql));
+            }
+            catch (Exception ex)
+            {
+                return Json(new{error=ex.Message});
+            }
+            finally
+            {
+                dapper.Dispose();
+            }
+        }
+                [HttpPost]
+        public async Task<IActionResult> comboSecciones(int idCarrera)
+        {
+            var dapper=new MySqlConnection(cn);
+            try
+            {
+                string sql=$@"select distinct(s.idSeccion),s.seccion
+                            from cursos n
+                            inner join carreras c on c.idCarrera = n.idCarrera
+                            inner join periodos_matriculas_niveles mn on mn.idNivel = n.idNivel
+                            inner join periodos p on p.idPeriodo = mn.idPeriodo
+                            inner join secciones s on s.idSeccion=mn.idSeccion
+                            where c.activa = 1 and p.permiteMatricula=1 and c.idCarrera=@idCarrera 
+                            {condicionPruebas}";
+                return Json(await dapper.QueryAsync(sql,new{idCarrera}));
+            }
+            catch (Exception ex)
+            {
+                return Json(new{error=ex.Message});
+            }
+            finally
+            {
+                dapper.Dispose();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> comboPeriodos(int idCarrera,int idSeccion)
+        {
+            var dapper=new MySqlConnection(cn);
+            try
+            {
+                string sql=$@"select distinct(p.idPeriodo),p.detalle,fecha_inicial
+                            from cursos n
+                            inner join carreras c on c.idCarrera = n.idCarrera
+                            inner join periodos_matriculas_niveles mn on mn.idNivel = n.idNivel
+                            inner join periodos p on p.idPeriodo = mn.idPeriodo
+                            inner join secciones s on s.idSeccion=mn.idSeccion
+                            where c.activa = 1 and p.permiteMatricula=1 and c.idCarrera=@idCarrera and s.idSeccion=@idSeccion
+                            {condicionPruebas}
+                            order by fecha_inicial";
+                return Json(await dapper.QueryAsync(sql,new{idCarrera,idSeccion}));
+            }
+            catch (Exception ex)
+            {
+                return Json(new{error=ex.Message});
+            }
+            finally
+            {
+                dapper.Dispose();
+            }
+        }
     }
 }
--- ISTPET_PortalEstudiantil/wwwroot/js/Inscripciones/siteInscripciones.js
+++ ISTPET_PortalEstudiantil/wwwroot/js/Inscripciones/siteInscripciones.js
@@ -1,6 +1,11 @@
-﻿window.addEventListener("load", () => {
+﻿const baseUrl = `${_route}Inscripciones/`;
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, 109);
     activarValidadores(frmDatos);
-})
+    comboCarreras();
+});
 
 function terminos() {
     formulario.hidden = !formulario.hidden;
@@ -21,6 +26,8 @@ async function siguiente(_from) {
             divAcademicos.hidden = true;
             divInscripcion.hidden = true;
             if (!await validarTodo(personales)) throw new Error("Verifique los campos requeridos");
+            if (await validarTodo(academicos)) divInscripcion.hidden = false;
+            limpiarValidadoresForm(academicos);
             tabAcademicos.click();
             divAcademicos.hidden = false;
         }
@@ -35,15 +42,97 @@ async function siguiente(_from) {
     }
 }
 
-async function cambiarTipoDocumento(){
+async function cambiarTipoDocumento() {
     idAlumno.removeAttribute("data-validate");
     idAlumno.classList.remove("is-invalid");
-    if(tipoDocumento.value=="C"){
-        idAlumno.setAttribute("data-validate","cedula");
+    if (tipoDocumento.value == "C") {
+        idAlumno.setAttribute("data-validate", "cedula");
         validarCedula(idAlumno);
-    }else{
+    } else {
         validarVacio(idAlumno);
     }
     activarValidadores(personales);
 
+}
+
+async function comboCarreras() {
+    try {
+        const url = `${baseUrl}comboCarreras`;
+        const res = (await axios.get(url)).data;
+        if (!!res.error) throw new Error(res.error);
+        let html = `<option value=''>Selecciona</option>`;
+        res.forEach(item => {
+            html += `<option value="${item.idCarrera}">${item.Carrera}</option>`;
+        });
+        idCarrera.innerHTML = html;
+    } catch (e) {
+        toastError(`${e.message}`);
+    }
+}
+
+async function comboSecciones() {
+    try {
+        if (idCarrera.value == "") {
+            idSeccion.innerHTML = "<option value=''>Selecciona una carrera</option>";
+            return;
+        }
+        const url = `${baseUrl}comboSecciones`;
+        const data = new FormData(frmDatos);
+        const res = (await axios.post(url, data)).data;
+        if (!!res.error) throw new Error(res.error);
+        let html = `<option value=''>Selecciona</option>`;
+        res.forEach(item => {
+            html += `<option value="${item.idSeccion}">${item.seccion}</option>`;
+        });
+        idSeccion.innerHTML = html;
+    } catch (e) {
+        toastError(`${e.message}`);
+    }
+}
+
+async function comboPeriodos() {
+    try {
+        if (idSeccion.value == "") {
+            idPeriodo.innerHTML = "<option value=''>Selecciona una sección</option>";
+            return;
+        }
+        const url = `${baseUrl}comboPeriodos`;
+        const data = new FormData(frmDatos);
+        const res = (await axios.post(url, data)).data;
+        if (!!res.error) throw new Error(res.error);
+        let html = `<option value=''>Selecciona</option>`;
+        moment.locale("es");
+        res.forEach(item => {
+            const fecha = moment(item.fecha_inicial).format("LLLL").split("0:00")[0];
+            html += `<option value="${item.idPeriodo}">${fecha.toUpperCase()} PERIODO: ${item.detalle}</option>`;
+        });
+        idPeriodo.innerHTML = html;
+    } catch (e) {
+        toastError(`${e.message}`);
+    }
+}
+
+async function guardar() {
+    try {
+        if (!await validarTodo(frmDatos)) throw new Error("Verifique todos los campos requeridos");
+        formulario.classList.remove(...formulario.classList);
+        formulario.classList.add("container-fluid");
+        formulario.innerHTML = `<div class='row bg-success text-center pt-28' style='height:100vh'>
+                                <div class='col-sm-12'>
+                                    <h1 class='text-white'>
+                                    <i class='bi-check-circle text-white' style='font-size:7rem'></i>
+                                    <br>
+                                    <br>
+                                    TÚ INSCRIPCIÓN EN LÍNEA SE REALIZÓ CORRECTAMENTE
+                                    </h1>  
+                                </div>
+                                <div class='col-sm-12'>
+                                    <button class='btn btn-light' onclick='top.location.reload()'>RELIZAR OTRA INSCRIPCIÓN</button>
+                                </div>
+                            </div>`;
+        const coeficiente = (formulario.getBoundingClientRect().height - window.screen.height) + 220;
+        window.scrollTo({ top: formulario.getBoundingClientRect().height - coeficiente })
+    } catch (e) {
+        toastError(`${e.message}`);
+    }
 }
